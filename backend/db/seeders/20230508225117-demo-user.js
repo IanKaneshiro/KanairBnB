3f927d29d45b2aab017e8771dc42a3af9e6eff67
ian <iandkaneshiro@gmail.com>
2023-05-08 15:57:27 -0700

    Add Users table migration and seeds

---
+++ backend/db/seeders/20230508225117-demo-user.js
@@ -0,0 +1,54 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = "Users";
+    return queryInterface.bulkInsert(
+      options,
+      [
+        {
+          firstName: "Bruce",
+          lastName: "Wayne",
+          email: "vengeance@user.io",
+          username: "LetsGetNuts",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          firstName: "Clark",
+          lastName: "Kent",
+          email: "farmboy@user.io",
+          username: "Smallville",
+          hashedPassword: bcrypt.hashSync("password2"),
+        },
+        {
+          firstName: "Diana",
+          lastName: "price",
+          email: "ww@user.io",
+          username: "ArtifactsLover",
+          hashedPassword: bcrypt.hashSync("password3"),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = "Users";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        username: { [Op.in]: ["LetsGetNuts", "Smallville", "ArtifactsLover"] },
+      },
+      {}
+    );
+  },
+};
