30a66796db6c8b4b8afdc3a4af2fafb25fe2a7a0
ian <iandkaneshiro@gmail.com>
2023-05-11 19:21:52 -0700

    Add model and migration for spots table. implemented Post route for spots

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const { check } = require("express-validator");
+
+const { handleValidationErrors } = require("../../utils/validation");
+const { requireAuth } = require("../../utils/auth");
+
+const { Spots } = require("../../db/models");
+
+const router = express.Router();
+
+const validateSpot = [
+  check("address")
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required."),
+  check("city").exists({ checkFalsy: true }).withMessage("City is required."),
+  check("state").exists({ checkFalsy: true }).withMessage("State is required."),
+  check("country")
+    .exists({ checkFalsy: true })
+    .withMessage("Country is required."),
+  check("lat")
+    .exists({ checkFalsy: true })
+    .withMessage("Latitude is not valid."),
+  check("lng")
+    .exists({ checkFalsy: true })
+    .withMessage("Longitude is not valid."),
+  check("name")
+    .exists({ checkFalsy: true })
+    .isLength({ max: 50 })
+    .withMessage("Name must be less than 50 characters."),
+  check("description")
+    .exists({ checkFalsy: true })
+    .withMessage("Description is required."),
+  check("price")
+    .exists({ checkFalsy: true })
+    .withMessage("Price per day is required."),
+  handleValidationErrors,
+];
+
+router.post("/", requireAuth, validateSpot, async (req, res, next) => {
+  const { address, city, state, country, lat, lng, name, description, price } =
+    req.body;
+
+  const spot = await req.user.createSpot({
+    ownerId: req.user.id,
+    address,
+    city,
+    state,
+    country,
+    lat,
+    lng,
+    name,
+    description,
+    price,
+  });
+
+  const safeSpot = {
+    id: spot.id,
+    ownerId: spot.ownerId,
+    address: spot.address,
+    city: spot.city,
+    state: spot.state,
+    country: spot.country,
+    lat: spot.lat,
+    lng: spot.lng,
+    name: spot.name,
+    description: spot.description,
+    price: spot.price,
+  };
+
+  res.status(201);
+  return res.json({
+    spot: safeSpot,
+  });
+});
+
+module.exports = router;
