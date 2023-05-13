ed8ec31afddaba94ec09ab382b484e7af179acfe
ian <iandkaneshiro@gmail.com>
2023-05-13 10:53:22 -0700

    Add contraints for create booking endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -75,79 +75,95 @@ router.post("/", requireAuth, validateSpot, async (req, res, next) => {
   return res.json(safeSpot);
 });
 
-// Book a spot based on spot idconst
+// Book a spot based on spot id
+const validateDate = [
+  check("endDate")
+    .exists({ checkFalsy: true })
+    .withMessage("endDate is required")
+    .isDate()
+    .withMessage("endDate must be a valid date.")
+    .custom((value, { req }) => {
+      if (value <= req.body.startDate) {
+        throw new Error("endDate cannot be on or before startDate");
+      }
+      return true;
+    }),
+  check("startDate")
+    .exists({ checkFalsy: true })
+    .withMessage("startDate is required")
+    .isDate()
+    .withMessage("startDate must be a valid date."),
+  handleValidationErrors,
+];
 
-router.post("/:spotId/bookings", requireAuth, async (req, res, next) => {
-  const { startDate, endDate } = req.body;
-  const spotId = parseInt(req.params.spotId);
-  const userId = req.user.id;
+router.post(
+  "/:spotId/bookings",
+  requireAuth,
+  validateDate,
+  async (req, res, next) => {
+    const { startDate, endDate } = req.body;
+    const spotId = parseInt(req.params.spotId);
+    const userId = req.user.id;
 
-  if (spotId === userId) {
-    const err = new Error("Cannot book your own spot");
-    err.status = 403;
-    return next(err);
-  }
-  const spot = await Spot.findByPk(spotId);
+    const spot = await Spot.findByPk(spotId);
 
-  if (!spot) {
-    // Error is the spot doesn't exist
-    const err = new Error("Spot couldn't be found");
+    // Error if the spot doesn't exist
+    if (!spot) {
+      const err = new Error("Spot couldn't be found");
 
-    err.status = 404;
-    return next(err);
-  }
+      err.status = 404;
+      return next(err);
+    }
 
-  // check if endDate is equal to or less than start date
-  if (endDate <= startDate) {
-    const err = new Error("Bad Request");
-    err.status = 400;
-    err.errors = {
-      endDate: "endDate cannot be on or before startDate",
-    };
-    return next(err);
-  }
+    // Authorization -- seperate middleware?
+    if (spot.ownerId === userId) {
+      const err = new Error("Forbidden");
+      err.status = 403;
+      return next(err);
+    }
 
-  // check if there is a existing booking
-  const existingBooking = await Booking.findOne({
-    where: {
-      userId,
+    // check if there is a existing booking
+    const existingBooking = await Booking.findOne({
+      where: {
+        userId,
+        spotId,
+        endDate: new Date(`${endDate}T00:00:00.000Z`).toISOString(),
+        startDate: new Date(`${startDate}T00:00:00.000Z`).toISOString(),
+      },
+    });
+
+    if (existingBooking) {
+      const err = new Error(
+        "Sorry, this spot is already booked for the specified dates"
+      );
+      err.status = 403;
+      err.errors = {
+        startDate: "Start date conflicts with an existing booking",
+        endDate: "End date conflicts with an existing booking",
+      };
+      return next(err);
+    }
+
+    const booking = await Booking.create({
       spotId: spot.id,
-      endDate: new Date(`${endDate}T00:00:00.000Z`).toISOString(),
-      startDate: new Date(`${startDate}T00:00:00.000Z`).toISOString(),
-    },
-  });
+      userId: userId,
+      startDate,
+      endDate,
+    });
 
-  if (existingBooking) {
-    const err = new Error(
-      "Sorry, this spot is already booked for the specified dates"
-    );
-    err.status = 403;
-    err.errors = {
-      startDate: "Start date conflicts with an existing booking",
-      endDate: "End date conflicts with an existing booking",
+    const safeBooking = {
+      id: booking.id,
+      userId: booking.userId,
+      spotId: booking.spotId,
+      startDate: booking.startDate,
+      endDate: booking.endDate,
+      createdAt: booking.createdAt,
+      updatedAt: booking.updatedAt,
     };
-    return next(err);
+
+    res.status(200);
+    return res.json(safeBooking);
   }
-
-  const booking = await Booking.create({
-    spotId: spot.id,
-    userId: userId,
-    startDate,
-    endDate,
-  });
-
-  const safeBooking = {
-    id: booking.id,
-    userId: booking.userId,
-    spotId: booking.spotId,
-    startDate: booking.startDate,
-    endDate: booking.endDate,
-    createdAt: booking.createdAt,
-    updatedAt: booking.updatedAt,
-  };
-
-  res.status(200);
-  return res.json(safeBooking);
-});
+);
 
 module.exports = router;
